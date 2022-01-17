ae6b4e4f35168015756ceeb3dc77396087226855
rdx6969 <82794243+rdx6969@users.noreply.github.com>
2022-01-17 19:06:25 +0530

    Add files via upload

---
+++ all.js
@@ -0,0 +1,75 @@
+document.getElementById('foter').innerHTML = `
+<div class="container-fluid border ">
+<div class="container text-center ">
+    <div class="row p-5">
+        <div class="col-lg-3 col-md-6 col-12 mt-1 text-center">
+            <p class="text-white mt-5">
+                About Meal
+            </p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum provident explicabo officia ut voluptate? Blanditiis ipsa sunt aspernatur autem nihil.</p>
+            <p class="text-white my-4">
+                Connect With Us
+            </p>
+            <div class="d-flex justify-content-center">
+                
+            <a  href="https://www.whatsapp.com/"><img src="img/wh.png" class="icon"></a>
+            <a  href="https://www.facebook.com/"><img src="img/facebook.png" class="icon"></a>
+            <a  href="https://www.instagram.com/"><img src="img/instagram.png" class="icon"></a>
+            <a  href="https://web.telegram.org"><img src="img/telegram.png" class="icon"></a>
+            <a  href="https://www.twitter.com/"><img src="img/twitter.png" class="icon"></a></li>
+            </div>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 text-center mt-5">
+            <p>Projects</p>
+            <p class=" mt-4">
+                Web Design
+            </p>
+            <p class=" mt-4">
+                HTML5
+            </p>
+            <p class=" mt-4">
+                Jquery
+            </p>
+            <p class=" mt-4">
+                Bootstrap
+            </p>
+            <p class=" mt-4">
+                JavaScript
+            </p>
+
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 text-center mt-5">
+            <p>Websites</p>
+            <p class=" mt-4">
+                Web Development
+            </p>
+            <p class=" mt-4">
+                PHP
+            </p>
+            <p class=" mt-4">
+                SQL
+            </p>
+            <p class=" mt-4">
+                Node.Js
+            </p>
+            <p class="mt-4">
+                Djhango
+            </p>
+        </div>
+        <div class="col-lg-3 col-md-6 col-sm-12 text-center mt-5">
+            <p class="mb-4">Conatact Us </p>
+            <p class="">At BaBa Ka DhaBa</p>
+            
+            <p class="">+91 696969696</p>
+            <p class="">+91 969696969</p>
+            <p class="">rdxninja1234@gmail.com</p>
+        </div>
+
+    </div>
+    <hr class="">
+    <p class="my-5"> Copyrights &copy;2022 All Rights Received | This Template is Made By &#9755;
+        <font class="text-neon-blue">Sanjay</font>
+    </p>
+</div>
+</div>
+`;
